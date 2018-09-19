b149f2a3ad414574acd54a858d7de37e79b81754
Patrik <36334711+patakrik@users.noreply.github.com>
2018-09-19 12:10:16 +0200

    Add files via upload

---
+++ Yoda/src/Yoda.java
@@ -0,0 +1,19 @@
+public class Yoda {
+
+ 
+    public static void main(String[] args) {
+        String myString = null;
+            if (myString.equals("foobar"))  // Erre kidobja a NullPointerException-t, viszont forditható
+           	//	{}
+            if(!"foobar".equals(myString))	 // Ez igaz lesz
+            {}
+                   
+             
+     
+            
+            
+                
+        
+    }
+    
+}
