6f79aeda9ae20f86452039fef0b9d8710ef4d3c3
Patrik <36334711+patakrik@users.noreply.github.com>
2018-09-26 13:16:06 +0200

    Add files via upload

---
+++ szulo/src/szulo/Gyerek.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szulo;
+
+public class Gyerek extends Szulo{
+     @Override
+     void viselkedes(){
+        System.out.print("nyavaja");
+    }
+
+}
---
+++ szulo/src/szulo/IIfeladat.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szulo;
+
+public class IIfeladat {
+
+    public static void main(String[] args) {
+        Szulo szulo = new Gyerek();
+            szulo.viselkedes();
+    }
+    
+}
